c6e6662b506f322f05aab7bab89e43c919404a22
wilsonconley <conleywilson0@gmail.com>
2025-06-07 18:57:17 -0400

    Add backend API for job listings

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
+
+const data = require("./jobs.json");
 
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
-
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json(data);
 });
