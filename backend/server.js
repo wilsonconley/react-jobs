1ca86f64d2d36c1f9cb46a983e06bdca2cda123a
wilsonconley <conleywilson0@gmail.com>
2025-06-08 18:30:11 -0400

    Add API to edit jobs

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,13 @@ app.get("/jobs/:id", (req, res) => {
   res.json(data.find((el) => el.id === id));
 });
 
+app.put("/jobs/:id", express.json(), (req, res) => {
+  const id = req.params.id;
+  const index = data.findIndex((job) => job.id === id);
+  data[index] = { ...req.body, ...{ id } };
+  res.send(`${id}`);
+});
+
 app.delete("/jobs/:id", (req, res) => {
   const id = req.params.id;
   data = data.filter((job) => job.id !== id);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 import type { JobApi } from "./components/JobListing";
 
 function App() {
@@ -22,6 +23,18 @@ function App() {
       body: JSON.stringify(newJob),
     });
     console.log("Added job", await res.json());
+    return;
+  };
+
+  const editJob = async (id: string, newJob: JobApi) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    console.log("Edited job", await res.json());
     return;
   };
 
@@ -41,6 +54,11 @@ function App() {
         <Route
           path="/jobs/:id"
           element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/jobs/edit/:id"
+          element={<EditJobPage editJobSubmit={editJob} />}
           loader={jobLoader}
         />
         <Route
